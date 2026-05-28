296656d5ab4569fa1850f06845f1e531f622f3ad
PabloCanto <pablogabriel17052005@gmail.com>
2026-05-28 17:07:48 -0600

    Fix: URL de RDS

--- Segunda_Entrega/DB.py
+++ Segunda_Entrega/DB.py
@@ -1,15 +1,20 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Formato: mysql+pymysql://USUARIO:PASSWORD@ENDPOINT_RDS:3306/NOMBRE_BD
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:AvenidaAlcorta@siceidb.cncta1grkkkn.us-east-1.rds.amazonaws.com:3306/siceidb"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("No se encontró la variable de entorno DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Esta función inyectará la sesión de la base de datos en tus endpoints
 def get_db():
     db = SessionLocal()
     try:
